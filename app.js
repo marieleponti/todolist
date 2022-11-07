d611f4241768ecf74bea4ac3df07615694a36fc8
Mariele Ponticiello <mponticiello@beltrailway.com>
2022-11-07 12:01:51 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('', function(req, res){
+    res.send("Hello.");
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000.");
+});
