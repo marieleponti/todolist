ed4761f9223cdfc0983031481b211c802b3be18c
Mariele Ponticiello <mponticiello@beltrailway.com>
2022-11-07 18:12:58 -0600

    added post and get to app.js, added array to store items, added styles

--- app.js
+++ app.js
@@ -5,8 +5,23 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const userlist = [];
+
 app.get('', function(req, res){
-    res.send("Hello.");
+const d = new Date();
+let day = d.getDay();
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+       
+    res.render('list', {today: d.toLocaleDateString("en-US", options), todo: userlist});
+});
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    userlist.push(item);
+    res.redirect('/');
 });
 
 app.listen(3000, function(){
